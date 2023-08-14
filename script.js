934caadae4f590a0169e984b021a8769638e1963
aniket30i <iamaniket.saha.09@protonmail.com>
2023-08-14 13:12:07 +0530

    highscore system implemented

--- script.js
+++ script.js
@@ -12,9 +12,8 @@
 // console.log(document.querySelector('.guess').value);
 
 let Secretnumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = Secretnumber;
-
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +27,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > Secretnumber) {
