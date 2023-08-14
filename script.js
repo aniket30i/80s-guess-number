300e4008163817490f404f024ff2a97473cc4252
aniket30i <iamaniket.saha.09@protonmail.com>
2023-08-14 13:42:49 +0530

    finished

--- script.js
+++ script.js
@@ -15,14 +15,18 @@ let Secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Bhosarpappu🤬';
+    displayMessage('Bhosarpappu🤬');
   } else if (guess === Secretnumber) {
-    document.querySelector('.message').textContent = 'Correct Number!🎉';
+    displayMessage('Correct Number!🎉');
     document.querySelector('.number').textContent = Secretnumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -35,12 +39,11 @@ document.querySelector('.check').addEventListener('click', function () {
     // When guess is too high
   } else if (guess !== Secretnumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > Secretnumber ? 'Too high📈' : 'Too Low📉';
+      displayMessage(guess > Secretnumber ? 'Too high📈' : 'Too Low📉');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lose😅';
+      displayMessage('You lose😅');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -72,6 +75,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.guess').value = '';
 });
